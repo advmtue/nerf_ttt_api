6f04c93a6b5d67c99fb14389e7d24a8dfcf462f4
adamtuechler <adamtuechler@gmail.com>
2020-09-08 20:16:00 +1000

    Add GamePlayer.anonymize

--- src/main/java/ch/adamtue/ttt/api/model/GamePlayer.java
+++ src/main/java/ch/adamtue/ttt/api/model/GamePlayer.java
@@ -1,5 +1,6 @@
 package ch.adamtue.ttt.api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import org.w3c.dom.Attr;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
@@ -51,12 +52,74 @@ public class GamePlayer {
     }
 
     /**
-     * Construct a range key for a large query space
+     * Construct a range key for non-player specific query
      *
      * @return DynamoDB range key for any game player
      */
     public static AttributeValue createEmptyRangeKey() {
         return AttributeValue.builder().s("PLAYER#").build();
+    }
+
+    /**
+     * Marshall a Query/GetItem response into a GamePlayer object
+     *
+     * @param item Query attribute map
+     * @return {GamePlayer} Game player object
+     */
+    public static GamePlayer createFromQuery(Map<String, AttributeValue> item) {
+        GamePlayer player = new GamePlayer();
+
+        // Always available
+        player.setPlayerId(item.get("sk").s().split("#")[1]);
+
+        if (item.containsKey("displayName")) {
+            player.setDisplayName(item.get("displayName").s());
+        }
+
+        if (item.containsKey("ready")) {
+            player.setReady(item.get("ready").bool());
+        }
+
+        if (item.containsKey("alive")) {
+            player.setAlive(item.get("alive").bool());
+        }
+
+        if (item.containsKey("role")) {
+            player.setRole(item.get("role").s());
+        }
+
+        return player;
+    }
+
+    /**
+     * Construct an anonymous (role/alive filtered) version of this player
+     *
+     * @param otherRole The other player's role
+     * @return {GamePlayer} New anonymized Game Player object
+     */
+    @JsonIgnore
+    public GamePlayer anonymize(String otherRole) {
+        GamePlayer gp = new GamePlayer();
+
+        // Always available
+        gp.setDisplayName(this.getDisplayName());
+        gp.setPlayerId(this.getPlayerId());
+        gp.setReady(this.isReady());
+        gp.setAlive(this.isAlive());
+
+        // If the role has been set
+        if (this.getRole() != null) {
+           // If local role is detective or the same as requesting player
+           if (this.getRole() == "DETECTIVE" || this.getRole() == otherRole) {
+               // Set role to current role
+               this.setRole(this.getRole());
+           } else {
+               // Hide role as innocent
+               this.setRole("INNOCENT");
+           }
+        }
+
+        return gp;
     }
 
     // Player ID
@@ -97,36 +160,5 @@ public class GamePlayer {
     }
     public void setAlive(boolean alive) {
         this.alive = alive;
-    }
-
-    /**
-     * Marshall a query map into a GamePlayer object
-     *
-     * @param item Query attribute map
-     * @return Game Player object
-     */
-    public static GamePlayer createFromQuery(Map<String, AttributeValue> item) {
-        GamePlayer player = new GamePlayer();
-
-        // Always available
-        player.setPlayerId(item.get("sk").s().split("#")[1]);
-
-        if (item.containsKey("displayName")) {
-            player.setDisplayName(item.get("displayName").s());
-        }
-        
-        if (item.containsKey("ready")) {
-            player.setReady(item.get("ready").bool());
-        }
-        
-        if (item.containsKey("alive")) {
-            player.setAlive(item.get("alive").bool());
-        }
-        
-        if (item.containsKey("role")) {
-            player.setRole(item.get("role").s());
-        }
-
-        return player;
     }
 }
